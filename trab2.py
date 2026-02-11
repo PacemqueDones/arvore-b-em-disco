5793288132be379096ac8b7dff7c162fc6de7e96
ander <anderson.vwd@gmail.com>
2026-02-11 11:52:41 -0300

    11-02-2026-12h

--- trab2.py
+++ trab2.py
@@ -4,11 +4,10 @@ from b_tree_disk import BTree
 caminho = "casos_teste/caso_teste_4.txt"
 
 d, f = ler_cabecalho(caminho)
-print(f'grau: d')
+print(f'grau: {d//2}')
 try:
-    with BTree(path="teste.bin", d=d, create=True) as arvore:
+    with BTree(path="teste.bin", d=d//2, create=True) as arvore:
         executar_operacoes(f, arvore)
+        arvore.print_tree_levels(arvore.raiz_idx)
 finally:
     f.close()
-
-arvore.print_tree_levels(arvore.raiz_idx)
