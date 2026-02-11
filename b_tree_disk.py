cc0a56bfc156b00dc3995e6eb45dbe6f5bef1e74
ander <anderson.vwd@gmail.com>
2026-02-11 12:01:30 -0300

    Restaura versão estável do arquivo arvore

--- b_tree_disk.py
+++ b_tree_disk.py
@@ -1,12 +1,9 @@
-from collections import deque
 from node import Node
 from storage import Storage
-from math import ceil
 
 class BTree:
-    def __init__(self, path: str, d: int, create: bool):
-        self.t = ceil(d/2)
-        self.d = d
+    def __init__(self, path: str, t: int, create: bool):
+        self.t = t
         self.storage = Storage(path, create=create)  # storage abre o arquivo
 
         if create:
@@ -14,23 +11,17 @@ class BTree:
             self.idx = 1                      # próximo índice livre
 
             raiz = Node(True, idx=self.raiz_idx)
-            self.storage.write_node(self.d, raiz)
-            self.storage.write_head(self.d, self.idx, self.raiz_idx)
+            self.storage.write_node(self.t, raiz)
+            self.storage.write_head(self.t, self.idx, self.raiz_idx)
 
         else:
-            self.d, self.idx, self.raiz_idx = self.storage.read_head()
-            self.t = ceil(self.d / 2)
+            self.t, self.idx, self.raiz_idx = self.storage.read_head()
+            #self.raiz = self.storage.read_node(self.t, self.raiz_idx)
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc, tb):
-        self.storage.close()
-        return False
 
     def search(self, chave: int, idx = None):
         node_idx = self.raiz_idx if idx is None else idx
-        node = self.storage.read_node(self.d, node_idx)
+        node = self.storage.read_node(self.t, node_idx)
 
         i = 0
         while i < node.n and chave > node.chaves[i]:
@@ -49,12 +40,11 @@ class BTree:
 
     def split(self, pai_idx: int, indice: int):
         t = self.t
-        d = self.d
 
         # y é um filho de pai, mas um filho cheio de chaves
-        pai = self.storage.read_node(d, pai_idx)
+        pai = self.storage.read_node(t, pai_idx)
         y_idx = pai.filhos[indice]
-        y = self.storage.read_node(d, y_idx)
+        y = self.storage.read_node(t, y_idx)
 
         # cria um novo nó.
         z = Node(y.folha, self.idx)
@@ -68,8 +58,8 @@ class BTree:
         pai.registros.insert(indice, y.registros[t - 1])
 
         # direita recebe parte alta
-        z.chaves = y.chaves[t: d - 1]
-        z.registros = y.registros[t:d - 1]
+        z.chaves = y.chaves[t: (2 * t) - 1]
+        z.registros = y.registros[t:(2 * t) - 1]
 
         # esquerda fica com parte baixa
         y.chaves = y.chaves[0: t - 1]
@@ -77,12 +67,12 @@ class BTree:
 
         # se y não é folha, reatribuimos os filhos de y em y e z
         if not y.folha:
-            z.filhos = y.filhos[t: d]
+            z.filhos = y.filhos[t: 2 * t]
             y.filhos = y.filhos[0: t]
 
-        self.storage.write_node(d, pai)
-        self.storage.write_node(d, y)
-        self.storage.write_node(d, z)
+        self.storage.write_node(t, pai)
+        self.storage.write_node(t, y)
+        self.storage.write_node(t, z)
 
     def insert(self, k: int, valor: int):
         t = self.t
@@ -423,33 +413,3 @@ class BTree:
         if not node.folha:
             for child_idx in node.filhos:
                 self.print_tree(child_idx, level + 1)
-
-    def print_tree_levels(self, root_idx: int):
-        def _fmt_node(node) -> str:
-            return "[" + ", ".join('key: ' + str(k) for k in node.chaves) + "]"
-
-        t = self.t
-        q = deque([(root_idx, 0)])
-
-        current_level = 0
-        line_parts = []
-
-        while q:
-            node_idx, level = q.popleft()
-            node = self.storage.read_node(t, node_idx)
-
-            # quando muda o nível, imprime a linha anterior
-            if level != current_level:
-                print(" ".join(line_parts))
-                line_parts = []
-                current_level = level
-
-            line_parts.append(_fmt_node(node))
-
-            if not node.folha:
-                for child_idx in node.filhos:
-                    q.append((child_idx, level + 1))
-
-        # imprime a última linha acumulada
-        if line_parts:
-            print(" ".join(line_parts))
