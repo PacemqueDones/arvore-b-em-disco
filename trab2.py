773ea598f5c96b83a8865f3e15be9db844467312
PacemqueDones <anderson.vwd@gmail.com>
2026-02-13 14:22:44 -0300

    13-02-2026-14:20

--- trab2.py
+++ trab2.py
@@ -8,11 +8,18 @@ import sys
 arquivo_entrada = "casos_teste/caso_teste_4.txt"
 arquivo_saida = 'output/teste.bin'
 
+# lê o valor de d (ordem máxima) e o arquivo com operações
 d, f = ler_cabecalho(arquivo_entrada)
-
+d=2
+print(d, (d + 1) // 2)
 try:
-    with BTree(path=arquivo_saida, t=d//2, create=True) as arvore:
+    # Calcule o grau mínimo (t) como o teto da metade de d.
+    # Isso permite aceitar casos em que o número máximo de filhos (d) é ímpar.
+    t_min = (d + 1) // 2
+    # Cria a árvore B com grau mínimo calculado
+    with BTree(path=arquivo_saida, t=t_min, create=True) as arvore:
         executar_operacoes(f, arvore)
+        # Imprime a árvore em níveis para depuração
         arvore.print_tree_levels(arvore.raiz_idx)
 finally:
     f.close()
